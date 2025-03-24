6b71ce5bbfc5056a7034620c09110dd65cf06604
valentino <valen3601@outlook.com>
2025-03-24 01:32:05 -0300

    generar contraseña

--- 02. JS Bases/generadorContrasenas.js
+++ 02. JS Bases/generadorContrasenas.js
@@ -1,37 +1,51 @@
 function checkLongitud(longitud) {
   /* TU CODIGO */
 
+  if (!longitud) return "debe ingresar la longitud"
+   if (typeof longitud !== "string") return "La longitud recibida no es válida"
+   if (longitud < 3) return  "La longitud debe ser mayor o igual a 3"
+   if (longitud > 10) return "La longitud debe ser menor o igual a 10"
+   return longitud
+
 }
 
 function generarContrasena(longitud, incluirEspeciales, incluirNumeros, incluirMayusculas ) {
   /* TU CODIGO */
-  var letras = null;
+  var letras =  "abcdefghijklmnñopqrstuvwxyz";
 
-  var numeros = null;
+  var numeros = 1234567890;
 
-  var especiales = null;
+  var especiales = "!#$%&/()=?¿*¨¨[]{}+-@\¡|°";
 
-  var letrasMayusculas = null
+  var letrasMayusculas = letras.toUpperCase()
 
-  var caracteresDisponibles = null;
-
-  if () {
+  var caracteresDisponibles = letras;
+  
+  if (incluirEspeciales) {
       
+    caracteresDisponibles += especiales
+  }
+  if (incluirNumeros) {
+    caracteresDisponibles += numeros
   }
 
-  if () {
-      
+  if (incluirMayusculas) {
+    caracteresDisponibles += letrasMayusculas
   }
 
-  if () {
-      
 
-  }
+  var contrasena = "";
 
-  var contrasena = null;
-
-  if () {
-
+  if (longitud >= 3) {
+    var índiceRandom = Math.floor(Math.random() * caracteresDisponibles.length)
+    var caracterRandom = caracteresDisponibles.charAt(índiceRandom)
+    contrasena = contrasena + caracterRandom
+    var índiceRandom = Math.floor(Math.random() * caracteresDisponibles.length)
+    var caracterRandom = caracteresDisponibles.charAt(índiceRandom)
+    contrasena = contrasena + caracterRandom
+    var índiceRandom = Math.floor(Math.random() * caracteresDisponibles.length)
+    var caracterRandom = caracteresDisponibles.charAt(índiceRandom)
+    contrasena = contrasena + caracterRandom
   }
 
   return null;

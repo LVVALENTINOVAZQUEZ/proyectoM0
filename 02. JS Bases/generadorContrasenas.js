e945d8008a42f9e4f29a63cfe6ae36f0972b81a3
valentino <valen3601@outlook.com>
2025-03-24 01:07:47 -0300

    Revert "actualización de generador de contraseña"
    This reverts commit ee9765089f9ff7bb51018eb20f5a9b3b08e6ec16.

--- 02. JS Bases/generadorContrasenas.js
+++ 02. JS Bases/generadorContrasenas.js
@@ -1,109 +1,44 @@
 function checkLongitud(longitud) {
   /* TU CODIGO */
-  if (!longitud) return "debe ingresar la longitud"
-  if (typeof longitud !== "string") return "La longitud recibida no es válida"
-  if (longitud < 3) return  "La longitud debe ser mayor o igual a 3"
-  if (longitud > 10) return "La longitud debe ser menor o igual a 10"
-  return longitud
-
 
 }
 
- function generarContrasena(longitud, incluirEspeciales, incluirNumeros, incluirMayusculas ) {
+function generarContrasena(longitud, incluirEspeciales, incluirNumeros, incluirMayusculas ) {
   /* TU CODIGO */
-  var letras = "abcdefghijklmnñopqrstuvwxyz"  ;
+  var letras = null;
 
-  var numeros = 1234567890;
+  var numeros = null;
 
-  var especiales = "!#$%&/()=?¿*¨¨[]{}+-@\¡|°";
+  var especiales = null;
 
-  var letrasMayusculas = letras.toUpperCase()
+  var letrasMayusculas = null
 
-  var caracteresDisponibles = letras;
+  var caracteresDisponibles = null;
 
-  if (incluirEspeciales) {
-    caracteresDisponibles += especiales
+  if () {
       
   }
 
-  if (incluirNumeros) {
-    caracteresDisponibles += numeros
+  if () {
       
   }
 
-  if (incluirMayusculas) {
-    caracteresDisponibles += letrasMayusculas
+  if () {
       
 
   }
 
-  var contrasena = "";
-    if (longitud >= 3) { 
-      var índiceRandom = Math.floor(Math.random() * caracteresDisponibles.length)
-      var caracterRandom = caracteresDisponibles.charAt(índiceRandom)
-      contrasena = contrasena + caracterRandom
-    
-    
-   }
-   if (longitud >= 4){
-    var índiceRandom = Math.floor(Math.random() * caracteresDisponibles.length)
-      var caracterRandom = caracteresDisponibles.charAt(índiceRandom)
-      contrasena = contrasena + caracterRandom
-   }
-      
-   if (longitud >= 5){
-    var índiceRandom = Math.floor(Math.random() * caracteresDisponibles.length)
-      var caracterRandom = caracteresDisponibles.charAt(índiceRandom)
-      contrasena = contrasena + caracterRandom
-    
-    
-   }
-   if (longitud >= 6){
-    var índiceRandom = Math.floor(Math.random() * caracteresDisponibles.length)
-      var caracterRandom = caracteresDisponibles.charAt(índiceRandom)
-      contrasena = contrasena + caracterRandom
-    
-    
-   }
-   if (longitud >= 7){
-    var índiceRandom = Math.floor(Math.random() * caracteresDisponibles.length)
-      var caracterRandom = caracteresDisponibles.charAt(índiceRandom)
-      contrasena = contrasena + caracterRandom
-    
-   
-   }
-   if (longitud >= 8){
-    var índiceRandom = Math.floor(Math.random() * caracteresDisponibles.length)
-      var caracterRandom = caracteresDisponibles.charAt(índiceRandom)
-      contrasena = contrasena + caracterRandom
-    
-     
-   }
-   if (longitud >= 9){
-    var índiceRandom = Math.floor(Math.random() * caracteresDisponibles.length)
-      var caracterRandom = caracteresDisponibles.charAt(índiceRandom)
-      contrasena = contrasena + caracterRandom
-    
-      
-   }
-   if (longitud >= 10){
-    var índiceRandom = Math.floor(Math.random() * caracteresDisponibles.length)
-      var caracterRandom = caracteresDisponibles.charAt(índiceRandom)
-      contrasena = contrasena + caracterRandom
-    
-      
-   }
-   
-   return "Contraseña generada: " + contrasena;
+  var contrasena = null;
+
+  if () {
+
   }
-  
 
-  
-
+  return null;
+}
 
 // <------- NO TOCAR -------->
 module.exports = {
   checkLongitud,
   generarContrasena,
 };
- 

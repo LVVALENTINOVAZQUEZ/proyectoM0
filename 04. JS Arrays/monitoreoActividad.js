964b98eaaae7bd56913021a7f27873b3b8003663
valentino <valen3601@outlook.com>
2025-03-30 02:39:48 -0300

    Subiendo los primeros ejercicios de arrays

--- 04. JS Arrays/monitoreoActividad.js
+++ 04. JS Arrays/monitoreoActividad.js
@@ -1,9 +1,14 @@
 // <------- Arreglo de actividades sospechozas -----> modificar el valor de ser necesario
-var actividadesSospechozas = null
+var actividadesSospechozas = []
 
 function agregarActividad(descripcion, nivelRiesgo){
     /* TU CODIGO */
+    if (!descripcion || !nivelRiesgo ) return "Descripcion o nivel de riesgo no valido"
+    if ( nivelRiesgo !== "bajo" && nivelRiesgo !== "medio" && nivelRiesgo !== "alto") return "Nivel de riesgo no valido, el nivel debe ser: bajo, medio o alto"
+    actividadesSospechozas.push("Descripcion: " + descripcion + ", Riesgo - " + nivelRiesgo)
+    return `Actividad: ${descripcion} con Nivel de riesgo: ${nivelRiesgo} fue agregada con exito`
     
+
 }
 
 function eliminarActividad(indice){
@@ -13,12 +18,30 @@ function eliminarActividad(indice){
 
 function filtrarActividadesPorRiesgo(nivelRiesgo){
     /* TU CODIGO */
+    if(!nivelRiesgo) return "Nivel de riesgo no valido"
+    if(nivelRiesgo !== "bajo" && nivelRiesgo !== "medio" && nivelRiesgo !== "alto") return  "Nivel de riesgo no valido, el nivel debe ser: bajo, medio o alto"
     
-}
+     var nuevoArray = actividadesSospechozas.filter(function(actividad){
+        if(actividad.includes(nivelRiesgo)) return actividad
+     })
+
+     if(nuevoArray.length > 0) return nuevoArray
+     return "No hay actividades con este nivel de riesgo"
+
+
+    }
+
 
 function generarReporteDeActividades(){
     /* TU CODIGO */
     
+    var nuevoArray = []
+    if (actividadesSospechozas.length > 0){
+        for (var i = 0; i < actividadesSospechozas.length; i++){
+            nuevoArray.push(`Id: ${i}, ${actividadesSospechozas[i]}`)
+        }
+            return nuevoArray
+    } else return "No hay actividades para mostrar"
 }
 
 // <------- NO TOCAR -------->

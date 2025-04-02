cef9e4f6d02a4948550114a44a1a33e508f90422
valentino <valen3601@outlook.com>
2025-04-02 01:44:38 -0300

    Subiendo códigos de objetos

--- 05. JS Objetos/gestionUsuarios.js
+++ 05. JS Objetos/gestionUsuarios.js
@@ -4,22 +4,39 @@ const { perfiles } = require("../build/js/perfiles.js")
 var asistente = {
     verPerfiles: function(opcion){
         /* TU CODIGO */
-      
+      if(opcion === "todo") return perfiles
+      if(opcion === "nombre") {
+        var nuevoArray = perfiles.map(function( ele){
+            return ele.usuario
+        })
+        return nuevoArray
+      }
+      if ( opcion === "codigo" || opcion === "antiguedad") return perfiles.map(perfil => perfil[opcion])
+      if (opcion === "nivel") return perfiles.map(perfil => perfil.nivel_de_autorizacion)
     },
     
     verPerfilesPorAntiguedad: function(){
         /* TU CODIGO */
-        
+        var nuevoArray = [...perfiles]
+        return nuevoArray.sort((a, b) => b.antiguedad - a.antiguedad)
     },
 
     verAdministradores: function(){
         /* TU CODIGO */
-
+        return perfiles.filter(ele => {
+            if(ele.nivel_de_autorizacion === "admin") return ele
+        })
     },
 
     modificarAcceso: function(usuario, codigo){
         /* TU CODIGO */
-        
+        var usuarioEncontrado = perfiles.find(ele => {
+            if(ele.usuario === usuario) return ele
+        })
+        if (!usuarioEncontrado) return "usuario no encontrado"
+        if (codigo.length !== 4 || isNaN(codigo)) return "codigo de acceso invalido, debe contener solo 4 numeros"
+        usuarioEncontrado.codigo = codigo
+        return "codigo de acceso modificado"
     }
 }
 
